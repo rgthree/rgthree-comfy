5769311aeab835fb27d6289308d468d443ce7641
rgthree <regis.gaughan@gmail.com>
2024-01-26 23:53:13 -0500

    Add sass and have better output for building.

--- __build__.py
+++ __build__.py
@@ -4,10 +4,13 @@ import subprocess
 import os
 from shutil import rmtree, copytree, ignore_patterns
 from glob import glob
-import json
+import time
 import re
 
+from py.log import COLORS
 from py.config import RGTHREE_CONFIG
+
+start = time.time()
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 DIR_SRC_WEB = os.path.abspath(f'{THIS_DIR}/src_web/')
@@ -16,15 +19,52 @@ DIR_WEB_COMFYUI = os.path.abspath(f'{DIR_WEB}/comfyui/')
 
 rmtree(DIR_WEB)
 
-copytree(DIR_SRC_WEB, DIR_WEB, ignore=ignore_patterns("typings*", "*.ts"))
 
-subprocess.run(["./node_modules/typescript/bin/tsc"])
+def log_step(msg=None, status=None):
+  """ Logs a step keeping track of timing and initial msg. """
+  global step_msg  # pylint: disable=W0601
+  global step_start  # pylint: disable=W0601
+  if msg:
+    step_msg = f'▻ [Starting] {msg}...'
+    step_start = time.time()
+    print(step_msg, end="\r")
+  elif status:
+    step_time = round(time.time() - step_start, 3)
+    if status == 'Error':
+      status_msg=f'{COLORS["RED"]}⤫ {status}{COLORS["RESET"]}'
+    else:
+      status_msg=f'{COLORS["BRIGHT_GREEN"]}🗸 {status}{COLORS["RESET"]}'
+    print(
+      f'{step_msg.ljust(50, ".")} {COLORS["BRIGHT_GREEN"]}🗸 {status}{COLORS["RESET"]} ({step_time}s)'
+    )
+
+
+log_step(msg='Copying web directory')
+copytree(DIR_SRC_WEB, DIR_WEB, ignore=ignore_patterns("typings*", "*.ts", "*.scss"))
+log_step(status="Done")
+
+log_step(msg='TypeScript')
+checked = subprocess.run(["./node_modules/typescript/bin/tsc"], check=True)
+log_step(status="Done")
+
+scsss = glob(os.path.join(DIR_SRC_WEB, "**", "*.scss"), recursive=True)
+log_step(msg=f'SASS for {len(scsss)} files')
+scsss = [i.replace(THIS_DIR, '.') for i in scsss]
+cmds = ["node", "./node_modules/sass/sass"]
+for scss in scsss:
+  out = scss.replace('src_web', 'web').replace('.scss', '.css')
+  cmds.append(f'{scss}:{out}')
+cmds.append('--no-source-map')
+checked = subprocess.run(cmds, check=True)
+log_step(status="Done")
 
 # Handle the common directories. Because ComfyUI loads under /extensions/rgthree-comfy we can't
 # easily share sources outside of the `DIR_WEB_COMFYUI` _and_ allow typescript to resolve them in
 # src view, so we set the path in the tsconfig to map an import of "rgthree/common" to the
 # "src_web/common" directory, but then need to rewrite the comfyui JS files to load from
 # "../../rgthree/common" (which we map correctly in rgthree_server.py).
+log_step(msg='Cleaning Imports')
+print('▻ [Starting] Cleaning Imports...', end="\r")
 web_subfolders = [f.name for f in os.scandir(DIR_WEB) if f.is_dir()]
 for subfolder in web_subfolders:
   js_files = glob(os.path.join(DIR_WEB, subfolder, '*.js'), recursive=True)
@@ -37,3 +77,6 @@ for subfolder in web_subfolders:
       filedata = re.sub(r'(from\s+["\'])rgthree/', '\\1../', filedata)
     with open(file, 'w', encoding="utf-8") as f:
       f.write(filedata)
+log_step(status="Done")
+
+print(f'Finished all in {round(time.time() - start, 3)}s')
--- py/log.py
+++ py/log.py
@@ -1,6 +1,6 @@
 # https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
 # https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
-COLORS_FG = {
+COLORS = {
   'BLACK': '\33[30m',
   'RED': '\33[31m',
   'GREEN': '\33[32m',
@@ -17,8 +17,7 @@ COLORS_FG = {
   'BRIGHT_MAGENTA': '\33[95m',
   'BRIGHT_CYAN': '\33[96m',
   'BRIGHT_WHITE': '\33[97m',
-}
-COLORS_STYLE = {
+  # Styles.
   'RESET': '\33[0m',
   'BOLD': '\33[1m',
   'NORMAL': '\33[22m',
@@ -27,45 +26,44 @@ COLORS_STYLE = {
   'BLINK': '\33[5m',
   'BLINK2': '\33[6m',
   'SELECTED': '\33[7m',
-}
-COLORS_BG = {
-  'BLACK': '\33[40m',
-  'RED': '\33[41m',
-  'GREEN': '\33[42m',
-  'YELLOW': '\33[43m',
-  'BLUE': '\33[44m',
-  'MAGENTA': '\33[45m',
-  'CYAN': '\33[46m',
-  'WHITE': '\33[47m',
-  'GREY': '\33[100m',
-  'BRIGHT_RED': '\33[101m',
-  'BRIGHT_GREEN': '\33[102m',
-  'BRIGHT_YELLOW': '\33[103m',
-  'BRIGHT_BLUE': '\33[104m',
-  'BRIGHT_MAGENTA': '\33[105m',
-  'BRIGHT_CYAN': '\33[106m',
-  'BRIGHT_WHITE': '\33[107m',
+  # Backgrounds
+  'BG_BLACK': '\33[40m',
+  'BG_RED': '\33[41m',
+  'BG_GREEN': '\33[42m',
+  'BG_YELLOW': '\33[43m',
+  'BG_BLUE': '\33[44m',
+  'BG_MAGENTA': '\33[45m',
+  'BG_CYAN': '\33[46m',
+  'BG_WHITE': '\33[47m',
+  'BG_GREY': '\33[100m',
+  'BG_BRIGHT_RED': '\33[101m',
+  'BG_BRIGHT_GREEN': '\33[102m',
+  'BG_BRIGHT_YELLOW': '\33[103m',
+  'BG_BRIGHT_BLUE': '\33[104m',
+  'BG_BRIGHT_MAGENTA': '\33[105m',
+  'BG_BRIGHT_CYAN': '\33[106m',
+  'BG_BRIGHT_WHITE': '\33[107m',
 }
 
 
 def log_node_success(node_name, message):
   """Logs a success message."""
-  _log_node(COLORS_FG["GREEN"], node_name, message)
+  _log_node(COLORS["GREEN"], node_name, message)
 
 
 def log_node_info(node_name, message):
   """Logs an info message."""
-  _log_node(COLORS_FG["CYAN"], node_name, message)
+  _log_node(COLORS["CYAN"], node_name, message)
 
 
 def log_node_warn(node_name, message):
   """Logs an warn message."""
-  _log_node(COLORS_FG["YELLOW"], node_name, message)
+  _log_node(COLORS["YELLOW"], node_name, message)
 
 
 def log_node(node_name, message):
   """Logs a message."""
-  _log_node(COLORS_FG["CYAN"], node_name, message)
+  _log_node(COLORS["CYAN"], node_name, message)
 
 
 def _log_node(color, node_name, message, prefix=''):
@@ -73,16 +71,15 @@ def _log_node(color, node_name, message, prefix=''):
 
 
 def _get_log_msg(color, node_name, message, prefix=''):
-  msg = f'{COLORS_STYLE["BOLD"]}{color}{prefix}[rgthree] {node_name.replace(" (rgthree)", "")}'
-  msg += f':{COLORS_STYLE["RESET"]} {message}'
+  msg = f'{COLORS["BOLD"]}{color}{prefix}[rgthree] {node_name.replace(" (rgthree)", "")}'
+  msg += f':{COLORS["RESET"]} {message}'
   return msg
 
 
 def log(message, color=None, msg_color=None, prefix=None):
-  color = COLORS_FG[color] if color is not None and color in COLORS_FG else COLORS_FG["BRIGHT_GREEN"]
-  msg_color = COLORS_STYLE["RESET"] if msg_color == 'RESET' else COLORS_FG[
-    msg_color] if msg_color is not None and msg_color in COLORS_FG else ''
+  color = COLORS[color] if color is not None and color in COLORS else COLORS["BRIGHT_GREEN"]
+  msg_color = COLORS[msg_color] if msg_color is not None and msg_color in COLORS else ''
   prefix = f'[{prefix}]' if prefix is not None else ''
   msg = f'{color}[rgthree]{prefix}'
-  msg += f'{msg_color} {message}{COLORS_STYLE["RESET"]}'
+  msg += f'{msg_color} {message}{COLORS["RESET"]}'
   print(msg)
