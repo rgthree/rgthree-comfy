6c5f7c95ed8487fe08e42bd0341cae6dc9c0f0ad
rgthree <regis.gaughan@gmail.com>
2025-05-11 22:10:00 -0400

    Add new files. (I guess -a doesn't add new files...)

---
+++ src_web/comfyui/image_or_latent_size.ts
@@ -0,0 +1,52 @@
+import type {LGraphNodeConstructor} from "@comfyorg/litegraph";
+import type {ComfyNodeDef} from "typings/comfy";
+
+import {app} from "scripts/app.js";
+import {RgthreeBaseServerNode} from "./base_node.js";
+import {NodeTypesString} from "./constants.js";
+import {ISerialisedNode} from "@comfyorg/litegraph/dist/types/serialisation.js";
+
+class RgthreeImageOrLatentSize extends RgthreeBaseServerNode {
+  static override title = NodeTypesString.IMAGE_OR_LATENT_SIZE;
+  static override type = NodeTypesString.IMAGE_OR_LATENT_SIZE;
+  static comfyClass = NodeTypesString.IMAGE_OR_LATENT_SIZE;
+
+  static override setUp(comfyClass: LGraphNodeConstructor, nodeData: ComfyNodeDef) {
+    RgthreeBaseServerNode.registerForOverride(comfyClass, nodeData, NODE_CLASS);
+  }
+
+  constructor(title = NODE_CLASS.title) {
+    super(title);
+  }
+
+  override onNodeCreated() {
+    super.onNodeCreated?.();
+
+    // Litegraph uses an array of acceptable input types, even though ComfyUI's types don't type
+    // it out that way.
+    this.addInput("input", ["IMAGE", "LATENT", "MASK"] as any);
+  }
+
+  override configure(info: ISerialisedNode): void {
+    super.configure(info);
+
+    if (this.inputs?.length) {
+      // Litegraph uses an array of acceptable input types, even though ComfyUI's types don't type
+      // it out that way.
+      this.inputs[0]!.type = ["IMAGE", "LATENT", "MASK"] as any;
+    }
+  }
+}
+
+/** An uniformed name reference to the node class. */
+const NODE_CLASS = RgthreeImageOrLatentSize;
+
+/** Register the node. */
+app.registerExtension({
+  name: "rgthree.ImageOrLatentSize",
+  async beforeRegisterNodeDef(nodeType: LGraphNodeConstructor, nodeData: ComfyNodeDef) {
+    if (nodeData.name === NODE_CLASS.type) {
+      NODE_CLASS.setUp(nodeType, nodeData);
+    }
+  },
+});
