5288408220180af41ce50b0d29135e1ef5f83fdb
rgthree <regis.gaughan@gmail.com>
2025-05-18 23:59:31 -0400

    Save the Display Any response to the pnginfo so it pre-exists when reloading the workflow.

--- py/display_any.py
+++ py/display_any.py
@@ -1,15 +1,6 @@
 import json
 from .constants import get_category, get_name
-
-
-class AnyType(str):
-  """A special class that is always equal in not equal comparisons. Credit to pythongosssss"""
-
-  def __ne__(self, __value: object) -> bool:
-    return False
-
-
-any = AnyType("*")
+from .utils import any_type, get_dict_value
 
 
 class RgthreeDisplayAny:
@@ -22,7 +13,11 @@ class RgthreeDisplayAny:
   def INPUT_TYPES(cls):  # pylint: disable = invalid-name, missing-function-docstring
     return {
       "required": {
-        "source": (any, {}),
+        "source": (any_type, {}),
+      },
+      "hidden": {
+        "unique_id": "UNIQUE_ID",
+        "extra_pnginfo": "EXTRA_PNGINFO",
       },
     }
 
@@ -30,7 +25,7 @@ class RgthreeDisplayAny:
   FUNCTION = "main"
   OUTPUT_NODE = True
 
-  def main(self, source=None):
+  def main(self, source=None, unique_id=None, extra_pnginfo=None):
     value = 'None'
     if isinstance(source, str):
       value = source
@@ -44,6 +39,13 @@ class RgthreeDisplayAny:
           value = str(source)
         except Exception:
           value = 'source exists, but could not be serialized.'
+
+    # Save the output to the pnginfo so it's pre-filled when loading the data.
+    if extra_pnginfo and unique_id:
+      for node in get_dict_value(extra_pnginfo, 'workflow.nodes', []):
+        if str(node['id']) == str(unique_id):
+          node['widgets_values'] = [value]
+          break
 
     return {"ui": {"text": (value,)}}
 
