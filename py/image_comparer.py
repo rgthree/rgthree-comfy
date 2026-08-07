c7f627ee616ffdcdd674df7aa2da04292e8bd8cc
W <2974831841@qq.com>
2026-08-08 02:12:25 +0800

    feat: add image_a output

--- py/image_comparer.py
+++ py/image_comparer.py
@@ -11,6 +11,9 @@ class RgthreeImageComparer(PreviewImage):
   FUNCTION = "compare_images"
   DESCRIPTION = "Compares two images with a hover slider, or click from properties."
 
+  RETURN_TYPES = ("IMAGE",)
+  RETURN_NAMES = ("image_a",)
+
   @classmethod
   def INPUT_TYPES(cls):  # pylint: disable = invalid-name, missing-function-docstring
     return {
@@ -34,9 +37,12 @@ class RgthreeImageComparer(PreviewImage):
 
     result = { "ui": { "a_images":[], "b_images": [] } }
     if image_a is not None and len(image_a) > 0:
-      result['ui']['a_images'] = self.save_images(image_a, filename_prefix, prompt, extra_pnginfo)['ui']['images']
+      result['ui']['a_images'] = self.save_images(image_a, filename_prefix, prompt, extra_pnginfo)['ui']['images'][cite: 3]
 
     if image_b is not None and len(image_b) > 0:
-      result['ui']['b_images'] = self.save_images(image_b, filename_prefix, prompt, extra_pnginfo)['ui']['images']
+      result['ui']['b_images'] = self.save_images(image_b, filename_prefix, prompt, extra_pnginfo)['ui']['images'][cite: 3]
 
-    return result
+    return {
+      "ui": result["ui"],
+      "result": (image_a,)
+    }
